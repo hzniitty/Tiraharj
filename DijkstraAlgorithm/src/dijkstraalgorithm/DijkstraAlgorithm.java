6f7bdc8e0a3c2c5bcf89ccf09a0b9bac9da9d3af
Hannu-Pekka Niittymaa <hzniitty@melkki.cs.helsinki.fi>
2012-05-21 12:49:46 +0300

    Lisätty DijkstraAlgorithm kansio

---
+++ DijkstraAlgorithm/src/dijkstraalgorithm/DijkstraAlgorithm.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dijkstraalgorithm;
+
+/**
+ *
+ * @author hzniitty
+ */
+public class DijkstraAlgorithm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
